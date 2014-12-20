4d7f8ea0382f5231859cd48b934d488741c04a1c
David Von Lehman <david.vonlehman@nordstrom.com>
2014-12-19 16:04:56 -0800

    Getting app to work in simulator mode

--- src/js/routes.js
+++ src/js/routes.js
@@ -4,20 +4,20 @@
  * Route configuration for the RDash module.
  */
 angular.module('RDash').config(['$stateProvider', '$urlRouterProvider',
-    function($stateProvider, $urlRouterProvider) {
+  function($stateProvider, $urlRouterProvider) {
 
-        // For unmatched routes
-        $urlRouterProvider.otherwise('/');
+    // For unmatched routes
+    $urlRouterProvider.otherwise('/');
 
-        // Application routes
-        $stateProvider
-            .state('index', {
-                url: '/',
-                templateUrl: 'templates/dashboard.html'
-            })
-            .state('tables', {
-                url: '/tables',
-                templateUrl: 'templates/tables.html'
-            });
-    }
+    // Application routes
+    $stateProvider
+      .state('index', {
+        url: '/',
+        templateUrl: 'templates/dashboard.html'
+      })
+      .state('tables', {
+        url: '/tables',
+        templateUrl: 'templates/tables.html'
+      });
+  }
 ]);
